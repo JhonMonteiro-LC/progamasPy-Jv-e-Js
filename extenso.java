69bdff3abeda94cb9fe81c3abcb800a3e46b082e
Jhon Monteiro <carneirojhon5@gmail.com>
2026-03-02 16:18:38 -0300

    Refactor Extenso class to extenso and clean code

--- extenso.java
+++ extenso.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Extenso {
+public class extenso {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -96,3 +96,4 @@ public class Extenso {
         sc.close();
     }
 }
+
