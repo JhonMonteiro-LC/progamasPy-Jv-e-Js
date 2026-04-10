c2daad16e136df90e07969f60f948613e7f9a1a5
Jhon Monteiro <carneirojhon5@gmail.com>
2026-04-10 07:52:46 -0300

    Delete codigo.java

--- codigo.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class codigo{
-public static void main (String[] args){
-String nome;
-Scanner scanner = new Scanner (System.in);
-nome = scanner.nextLine();
-    System.out.println(nome);
-}
-}
